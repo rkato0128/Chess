d9fa5e2e5337ebd896afe7063bfb111390d75e74
Dong-yeob <rkato0128@naver.com>
2022-08-11 22:24:25 +0900

    킹, 퀸, 비숍 추가

---
+++ Project MC/Assets/Scripts/Piece_King.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece_King : ChessPiece
+{
+    public override void CheckPath()
+    {
+        // Vertical and horizontal
+        CheckGeneralPath(currentTile.coordinate, Direction.NEGATIVE, Direction.ZERO);
+        CheckGeneralPath(currentTile.coordinate, Direction.POSITIVE, Direction.ZERO);
+        CheckGeneralPath(currentTile.coordinate, Direction.ZERO, Direction.POSITIVE);
+        CheckGeneralPath(currentTile.coordinate, Direction.ZERO, Direction.NEGATIVE);
+        
+        // Diagonol
+        CheckGeneralPath(currentTile.coordinate, Direction.NEGATIVE, Direction.POSITIVE);
+        CheckGeneralPath(currentTile.coordinate, Direction.POSITIVE, Direction.POSITIVE);
+        CheckGeneralPath(currentTile.coordinate, Direction.NEGATIVE, Direction.NEGATIVE);
+        CheckGeneralPath(currentTile.coordinate, Direction.POSITIVE, Direction.NEGATIVE);
+    }
+}
---
+++ Project MC/Assets/Scripts/Piece_Queen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece_Queen : ChessPiece
+{
+    public override void CheckPath()
+    {
+        // Vertical and horizontal
+        CheckGeneralPath(currentTile.coordinate, Direction.NEGATIVE, Direction.ZERO, BM.boardManager.size.x);
+        CheckGeneralPath(currentTile.coordinate, Direction.POSITIVE, Direction.ZERO, BM.boardManager.size.x);
+        CheckGeneralPath(currentTile.coordinate, Direction.ZERO, Direction.POSITIVE, BM.boardManager.size.y);
+        CheckGeneralPath(currentTile.coordinate, Direction.ZERO, Direction.NEGATIVE, BM.boardManager.size.y);
+        
+        // Diagonol
+        CheckGeneralPath(currentTile.coordinate, Direction.NEGATIVE, Direction.POSITIVE, BM.boardManager.size.x);
+        CheckGeneralPath(currentTile.coordinate, Direction.POSITIVE, Direction.POSITIVE, BM.boardManager.size.x);
+        CheckGeneralPath(currentTile.coordinate, Direction.NEGATIVE, Direction.NEGATIVE, BM.boardManager.size.y);
+        CheckGeneralPath(currentTile.coordinate, Direction.POSITIVE, Direction.NEGATIVE, BM.boardManager.size.y);
+    }
+}
