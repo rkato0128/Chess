f2781987b28c49276283a4322958a524c8d84bd3
Dong-yeob <rkato0128@naver.com>
2024-09-27 16:59:35 +0900

    DeckBuilding - Card UI 작업중

--- Project MC/Assets/Scripts/DeckBuilding/Card.cs
+++ Project MC/Assets/Scripts/DeckBuilding/Card.cs
@@ -2,28 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
     public Constants.PieceType type;
+    public int cardAmount = 0;
 
     [SerializeField] private Button buttonIncrease;
     [SerializeField] private Button buttonDecrease;
+    [SerializeField] private TextMeshProUGUI cardName;
+    [SerializeField] private TextMeshProUGUI cardAmountText;
 
 
     private void Awake()
     {
         buttonIncrease.onClick.AddListener(Increase);
         buttonDecrease.onClick.AddListener(Decrease);
+
+        // Initialize Value
+        cardName.text = type.ToString();
+        cardAmountText.text = cardAmount.ToString();
     }
 
     private void Increase()
     {
-
+        // Max 값 가져와서 limit 처리 
+        cardAmount++;
     }
 
     private void Decrease()
     {
-
+        // 0 이하로 못 내려가도록 처리 
+        cardAmount--;
     }
 }
